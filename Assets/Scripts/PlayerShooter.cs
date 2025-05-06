3ceb24d2ce660042cbf99d003832ba05db0a105e
khwanrudeepuen <khwanrudee.puen@bumail.net>
2025-05-06 11:24:07 +0700

    Add Scripts/PlayerShooter

---
+++ Assets/Scripts/PlayerShooter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform shootPoint;
+    public float bulletSpeed = 10f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //  คลิกซ้ายเพื่อยิง
+        {
+            GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.linearVelocity = Vector2.right * bulletSpeed; // กระสุนวิ่งไปทางขวา
+        }
+    }
+}
